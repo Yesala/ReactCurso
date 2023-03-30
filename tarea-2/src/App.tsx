b7c5592eb9915fc9bac0f1b7dca452f3e18321d3
Yesala <ysalazarj@ucenfotec.ac.cr>
2023-03-30 10:00:17 -0600

    Tarea-2 Initial Commit

--- tarea-2/src/App.tsx
+++ tarea-2/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ListaPublicacionesWrapper } from "./components/organisms/ListaPublicacionesWrapper";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <ListaPublicacionesWrapper />
+    </div>
   );
 }
 
