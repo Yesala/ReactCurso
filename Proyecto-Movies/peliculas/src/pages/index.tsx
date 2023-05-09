5180bd6fe42e9d6977e9ba523e5ec0265001583c
Yesala <ysalazarj@ucenfotec.ac.cr>
2023-05-08 19:31:04 -0600

    Cambios Styles-Index

--- Proyecto-Movies/peliculas/src/pages/index.tsx
+++ Proyecto-Movies/peliculas/src/pages/index.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
-
 import { ProtectedPage } from "@/components/layouts/ProtectedPage";
 import { usePeliculasContext } from "@/contexts/peliculas-context";
 import { useFirebaseAuth } from "@/contexts/firebase-auth-context";
 import { saveFavoriteMovie } from "@/services/firebase";
 import ReactStars from "react-stars";
 import Link from "next/link";
+import { Modal, useModal, Button, Text } from "@nextui-org/react";
 
 export default function Home() {
   const { popularMovies } = usePeliculasContext();
   const { user } = useFirebaseAuth();
+  const { setVisible, bindings } = useModal();
 
   return (
     <ProtectedPage>
@@ -37,15 +38,58 @@ export default function Home() {
               <div className="rating">
                 <ReactStars count={5} size={24} color2={"#ffd700"} />
               </div>
-              <div></div>
+              <div className="review">
+                <Button
+                  className="btn-comments"
+                  auto
+                  flat
+                  onPress={() => setVisible(true)}
+                >
+                  Comentarios
+                </Button>
+                <Modal
+                  scroll
+                  width="600px"
+                  closeButton
+                  aria-labelledby="modal-title"
+                  aria-describedby="modal-description"
+                  {...bindings}
+                >
+                  <Modal.Header>
+                    <Text id="modal-title" size={30}>
+                      Review
+                    </Text>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <input
+                      className="border rounded shadow"
+                      type="text"
+                      id="review"
+                      name="review"
+                      placeholder="Escriba su opinión aquí"
+                    />
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      flat
+                      auto
+                      color="error"
+                      onPress={() => setVisible(false)}
+                    >
+                      Cerrar
+                    </Button>
+                    <Button onPress={() => setVisible(false)}>Enviar</Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
               <button
                 className="bg-white py-2 px-4 border rounded shadow"
                 onClick={() => user && saveFavoriteMovie(movie, user)}
                 style={{
                   fontSize: "13px",
                   color: "gray",
-                  marginLeft: "25px",
-                  marginBottom: "15px",
+                  marginLeft: "28px",
+                  marginBottom: "20px",
                 }}
               >
                 Guardar en Favoritos
